f031e716432308d98ebe2e17148f6325fc1c90ca
lowrck <lowrck@gmail.com>
2018-08-26 16:54:43 -0500

    finished methods now onto implementation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,25 @@
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        boolean done = false;
+    public static void main(String[] args) throws FileNotFoundException {
+        //variables
+        boolean done1 = false, done2 = false, done3 = false;
+        int ic = 0;
+        int[] intin = new int[5000];
+        int[] intout = new int[5000];
+        String userName;
+        File inputFile = null;
+        //Scanners
         Scanner input = new Scanner(System.in);
-        while(done != true) {
+        Scanner s = null;
+
+        while(done1 != true) {
             System.out.println("Please enter file address: ");
-            File inputFile = new File(input.next());
+            inputFile = new File(input.next());
             // Inputs
             try {
                 Scanner file = new Scanner(inputFile);
@@ -17,7 +27,20 @@ public class Main {
                 System.out.println("The file address is invalid please enter a valid path");
                 continue;
             }
-        done = true;
+        done1 = true;
+        }
+        s = new Scanner(inputFile);
+        while (s.hasNextInt()) {
+            intin[ic] = s.nextInt();
+            ic++;
+        }
+
+        System.out.println("Number of matching items: " + numSort(intin, intout));
+        for(int output : intout) {
+            if(output != 0) {
+                System.out.println(output);
+            }
+
         }
 
 
@@ -27,5 +50,31 @@ public class Main {
 
 
 
+    }
+// Method to pass a user determined signed integer from one array to another
+    static int numSort(int[] input, int[] output){
+        Scanner in = new Scanner(System.in);
+        int counterOutput = 0;
+        System.out.println("Enter 1 for all positive, enter 2 for all negative");
+        int menu = in.nextInt();
+        for(int i = 0; i < input.length; i++){
+            if(menu == 1) { if(input[i] > 0) { output[counterOutput++] = input[i]; } }
+            else if(menu == 2) { if(input[i] < 0) { output[counterOutput++] = input[i]; } }
+            else{ i--;
+            System.out.println(
+                    menu + " is not a valid menu option please enter 1 for all positive, or 2 for all negative");
+            menu = in.nextInt();
+            }
+        } return counterOutput;
+    }
+    static String userName() {
+        Scanner in = new Scanner(System.in);
+        String firstName;
+        String lastName;
+        System.out.println("Please enter your name with space between first and last name");
+        firstName = in.next();
+        lastName = in.next();
+        System.out.println( "Your name is: " + firstName + lastName);
+        return firstName+lastName;
     }
 }
